0b64a611e35e0a7b6ba8e3bfb80105ccbaecdcf8
Daniel Kaplun <41114118+FeeeLyX@users.noreply.github.com>
2021-01-08 13:46:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from PyQt5.QtWidgets import (QMainWindow, QWidget, QPushButton, QHBoxLayout, QVB
 from PyQt5.QtGui import QFont, QIcon, QPixmap, QPainter, QColor, QPen
 from PyQt5.QtCore import Qt
 import random
+import time
 
-# class map(QWidget):
-#     def 
 class ListRobots(QWidget):
     def __init__(self):
         super().__init__()
@@ -21,28 +20,36 @@ class ListRobots(QWidget):
             vbox.addLayout(tmp)
         self.setLayout(vbox)
 
+    # генерация списка строк
     def genList(self,amount):
         hbox = []
         for i in range(amount):
             hbox.append(self.genStrRbt(i))
-            # myWidget = hbox[i].itemAt(2).widget()
         return hbox
 
-    def genStrRbt(self,i):
+    # интерфейс строки списка ботов
+    def genStrRbt(self, i):
         hbox = QHBoxLayout()
-        hbox.addStretch(1)
  
-        id = QLabel(str(i+1))
+        id = QLabel(('0' if (i + 1 < 10) else '') + str(i + 1))
+        id.setFixedWidth(20)
+        id.move(0, 0)
 
-        osInf = QLabel("Linux")
+        osInf = QLabel("")
+        osInf.setFixedWidth(100)
+        osInf.move(id.width() + 10, 0)
 
         power = QProgressBar()
+        power.setFixedWidth(100)
+        power.move(id.width() + osInf.width() + 20, 0)
 
         U = QLabel(str(20))
 
-        permissionMotion = QPushButton('Разрешение движения')
+        permissionMotion = QPushButton('Разрешить движение')
+        permissionMotion.setFixedWidth(120)
        
         STOP = QPushButton('Аварийная остановка')
+        STOP.setFixedWidth(120)
        
         hbox.addWidget(id)                  #1
         hbox.addWidget(osInf)               #2
@@ -53,81 +60,94 @@ class ListRobots(QWidget):
 
         return hbox
 
-    def setText(self,column,row,text):
-        myFirstLayout = self.layout()
-        myLayout = myFirstLayout.itemAt(row).layout()
-        myWidget = myLayout.itemAt(column).widget()
+    def setText(self, column, row, text):
+        myLayout = self.layout()
+        rowLayout = myLayout.itemAt(row).layout()
+        myWidget = rowLayout.itemAt(column).widget()
         myWidget.setText(text)
 
-    def setValue(self,column,row,text):
-        myFirstLayout = self.layout()
-        myLayout = myFirstLayout.itemAt(row).layout()
-        myWidget = myLayout.itemAt(column).widget()
-        myWidget.setValue(text)
+    def setValue(self, column, row, value):
+        myLayout = self.layout()
+        rowLayout = myLayout.itemAt(row).layout()
+        myWidget = rowLayout.itemAt(column).widget()
+        myWidget.setValue(value)
 
     # def buttonClicked(self):
     #     sender = self.sender()
     #     Message = QMessageBox.question(self, 'Message',"Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
-
-
 class Application(QWidget):
     def __init__(self):
         super().__init__()
         
-        # potok = threading.Thread(target= self.read_sok)
+        # potok = threading.Thread(target=self.read_sok)
         # potok.start()
         self.initUI()
 
-    def initUI(self):
+    def updateRow(self, index, data):
+        sysinfo = data[0]
+        self.lr.setText(1, index, sysinfo)
+        capacity = data[1]
+        self.lr.setValue(2, index, capacity)
+        voltage = ('0' if data[2]<10 else '') + str(data[2]) + ' В'
+        self.lr.setText(3, index, voltage)
 
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('web.png'))
+    def initUI(self):
+        self.setGeometry(100, 100, 300, 220)
+        self.setWindowTitle('Jetbot Dashboard')
+        self.setWindowIcon(QIcon('icon.png'))
         self.show()
 
-        lr = ListRobots()
-        
-        #Получаем доступ к данным в списке роботов
-        
+        self.lr = ListRobots()
 
         scroll = QScrollArea()  
-        scroll.setWidget(lr)
-        scroll.setFixedWidth(500)
-        scroll.move(0,0)
+        scroll.setWidget(self.lr)
+        scroll.setFixedWidth(550)
+        scroll.move(0, 0)
         scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        qp = QPainter()
-        qp.setPen(Qt.red)
 
-        for i in range(100):
-            x = random.randint(1, 500-1)
-            y = random.randint(1, 500-1)
-            qp.drawPoint(x, y)
+        #qp = QPainter()
+        #qp.setPen(Qt.red)
+        #for i in range(30):
+        #    x = random.randint(1, 500-1)
+        #    y = random.randint(1, 500-1)
+        #    qp.drawPoint(x, y)
 
+        # плейсхолдер миникарты
         picture = QLabel(self)
-        pixmap = QPixmap('web.png')
+        pixmap = QPixmap('icon.png')
         picture.setPixmap(pixmap)
 
+        # настройка расположения элементов
         hbox = QHBoxLayout()
         hbox.addWidget(scroll)
         hbox.addWidget(picture)
         self.setLayout(hbox)
 
-        lr.setValue(3,2,45)
+        # настройка размеров окна
+        self.setMinimumWidth(1400)
+        self.setMinimumHeight(910)
 
-        self.setMinimumWidth(1000)
-        self.setMinimumHeight(500)
     # def read_sok(self):
     #     while 1 :
     #         data = sor.recv(1024)
     #         print(data.decode('utf-8'))
 
-
-
 if __name__ == '__main__':
-
-
     app = QApplication(sys.argv)
     ex = Application()
+
+    # ToDo: вынести обновление в функцию по таймеру
+    for i in range(30):
+        # плейсхолдер данных с робота
+        # ToDo: получать данные с сервера
+        capacity = random.randint(0, 100)
+        voltage = 9 + (capacity * 3)//100
+        data = ['Ubuntu 18.04 LTS' if random.randint(0,1)==0 else 'Windows 10 Pro', capacity, voltage]
+
+        ex.updateRow(i+1, data)
+
     sys.exit(app.exec_())
+
+    
