1246021317fa1e6c25deed6f3f50da0d580d6fdc
Daniel Kaplun <41114118+FeeeLyX@users.noreply.github.com>
2021-01-10 06:35:33 +0300

    reconnection support pt.2

--- node.py
+++ node.py
@@ -1,19 +1,40 @@
 import socket
 import random
+import time
+import threading
 
-sock = socket.socket()
-sock.connect(('192.168.1.68', 9090))
+def response():
+    serverIP = ('192.168.1.68', 9090)
+    
+    while 1:
+        sock = socket.socket()
+        try:
+            sock.connect(serverIP)
+        except TimeoutError as e:
+            print(e)
+            continue
 
-while 1:
-    data = sock.recv(1024)
-    if not data:
-        continue
-    else:
-        print('получен ' + data.decode() + ' от сервера')
+        while 1:
+            try:
+                data = sock.recv(1024)
+            except ConnectionResetError as e:
+                print(e)
+                sock.close()
+                break
+            
+            if not data:
+                continue
+            else:
+                print('получен ' + data.decode() + ' от сервера')
 
-    charge = random.randint(0, 100)
-    voltage = 9 + (charge * 3) // 100
-    x = random.randint(0, 50)
-    y = random.randint(0, 50)
-    s = 'Ubuntu 18.04 LTS, ' + str(charge) + ', ' + str(voltage) + ', ' + str(x) + ', ' + str(y) 
-    sock.send(s.encode())
+            charge = random.randint(0, 100)
+            voltage = 9 + (charge * 3) // 100
+            x = random.randint(0, 49)
+            y = random.randint(0, 49)
+            s = 'Ubuntu 18.04 LTS, ' + str(charge) + ', ' + str(voltage) + ', ' + str(x) + ', ' + str(y) 
+            sock.send(s.encode())
+            print('ответ: ' + s)
+
+if __name__ == '__main__':
+    threading.Thread(target=response, daemon=True).start()
+    
